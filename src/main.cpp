5311e2bb1f74b2dedd03d250a63e93b3a2bf90b5
Christiaan Herrewijn <christiaan@duckdblabs.com>
2025-03-17 17:08:52 +0100

    test with multi-statement query

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,24 @@
 
 #include <iostream>
 
-int main() {
-	duckdb::DuckDB db(nullptr);
-	duckdb::Connection con(db);
-	std::string query = "SELECT 42;";
-	duckdb::unique_ptr<duckdb::MaterializedQueryResult> q_result = con.Query(query);
+using namespace duckdb;
 
-	std::cout << q_result->ToString() << std::endl;
+int main() {
+
+	DuckDB db(nullptr);
+	Connection con(db);
+	std::string query = "SELECT 42 as my_col; SELECT 43 as my_other_col;";
+
+	unique_ptr<MaterializedQueryResult> q_result = con.Query(query);
+
+	// query 1
+	std::cout << q_result->names[0] << std::endl;
+	std::cout << q_result->GetValue(0, 0) << std::endl;
+	std::cout << q_result->types[0].ToString() << std::endl;
+
+	// query 2
+	auto q2 = unique_ptr_cast<QueryResult, MaterializedQueryResult>(std::move(q_result->next));
+	std::cout << q2->names[0] << std::endl;
+	std::cout << q2->GetValue(0, 0) << std::endl;
+	std::cout << q2->types[0].ToString() << std::endl;
 }
